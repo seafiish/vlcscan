f02d8df3685bfa955723652a65fc66ebbade55d6
seafiish <seafiish@proton.me>
2025-06-17 13:17:56 -0500

    added stuff to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 sudo raspi-config nonint do_boot_behavior B2
-#sudo raspi-config nonint do_audio
+sudo raspi-config nonint do_audio vc4-hdmi
+
 sudo apt update
 sudo apt --full-upgrade
 sudo apt install python3
 sudo apt install vlc
+
+venv/bin/pip3 install python-vlc
+
+#TODO add wayland stuff?
