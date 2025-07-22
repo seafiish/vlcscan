b2e3791c198eefde507e3cf0810b8e549c699e0d
seafiish <seafiish@proton.me>
2025-07-22 00:18:03 -0500

    updated install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo raspi-config nonint do_boot_behaviour B2
 sudo raspi-config nonint do_audio 1
 
 sudo apt update
-sudo apt --full-upgrade
+sudo apt full-upgrade
 sudo apt install python3
 sudo apt install python3-pip
 sudo apt install vlc
